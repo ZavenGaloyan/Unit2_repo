6aa4357dccd045c7cb13d0a91e233fc360c6c240
ZavenGaloyan <111752809+ZavenGaloyan@users.noreply.github.com>
2022-11-17 22:23:49 +0900

    Create Humidity_Graph.m

---
+++ Lessons/Humidity_Graph.m
@@ -0,0 +1,5 @@
+Humidity gragh assignment
+Code and Graph below
+![](https://github.com/ZavenGaloyan/Unit2_repo/blob/main/Lessons/Humidity_code_1.jpg)
+![](https://github.com/ZavenGaloyan/Unit2_repo/blob/main/Lessons/Humidity_code_2.jpg)
+![](https://github.com/ZavenGaloyan/Unit2_repo/blob/main/Lessons/humidity_graph.png)
